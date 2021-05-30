f1b2759f308eb0ee3b60410dd8d3c371a7fae6f7
qianhk <qhk83@qq.com>
2021-05-30 12:36:15 +0800

    test DecoratedBox Transform actions drawer bottomNavigationBar

--- kai_flutter_demo_module/lib/test_page_2.dart
+++ kai_flutter_demo_module/lib/test_page_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class TestPage2 extends StatefulWidget {
   TestPage2({Key key, this.title = "Test 02"}) : super(key: key);
@@ -12,19 +13,116 @@ class _TestPage2State extends State<TestPage2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Text:',
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: Container(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
+          margin: EdgeInsets.all(0),
+          constraints: BoxConstraints.expand(),
+          color: Colors.green[50],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Text2:',
+              ),
+              DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [Colors.red, Colors.orange[700]]),
+                    borderRadius: BorderRadius.circular(4),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black54,
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 4.0,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+              Container(
+                color: Colors.blue[100],
+                child: Transform(
+                  alignment: Alignment.bottomRight, //相对于坐标系原点的对齐方式
+                  transform: Matrix4.skewY(0.3), //沿Y轴倾斜0.3弧度
+                  child: Container(
+                    margin: EdgeInsets.only(top: 80),
+                    padding: const EdgeInsets.all(8.0),
+                    color: Colors.deepOrange,
+                    child: const Text('Apartment'),
+                  ),
+                ),
+              ),
+              DecoratedBox(
+                decoration: BoxDecoration(color: Colors.red),
+                child: Transform.translate(
+                  offset: Offset(10.0, 10),
+                  child: Text("Hello world"),
+                ),
+              ),
+              DecoratedBox(
+                decoration: BoxDecoration(color: Colors.green),
+                position: DecorationPosition.background,
+                child: Transform.rotate(
+                  angle: math.pi / 4,
+                  child: Text(
+                    "Hello world",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.red),
+                    child: Transform.scale(scale: 1.5, child: Text("Hello world")),
+                  ),
+                  Text(
+                    "你好",
+                    style: TextStyle(color: Colors.green, fontSize: 18.0),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.red),
+                    //将Transform.rotate换成RotatedBox
+                    child: RotatedBox(
+                      quarterTurns: 1, //旋转90度(1/4圈)
+                      child: Text("Hello world"),
+                    ),
+                  ),
+                  Text(
+                    "你好",
+                    style: TextStyle(color: Colors.green, fontSize: 18.0),
+                  )
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50.0, left: 0.0),
+                constraints: BoxConstraints.tightFor(width: 200.0, height: 150.0),
+                decoration: BoxDecoration(
+                    gradient: RadialGradient(colors: [Colors.red, Colors.orange], center: Alignment.topLeft, radius: .98),
+                    boxShadow: [BoxShadow(color: Colors.black54, offset: Offset(2.0, 2.0), blurRadius: 4.0)]),
+                transform: Matrix4.rotationZ(.0),
+                alignment: Alignment.center,
+                child: Text(
+                  "Kai",
+                  style: TextStyle(color: Colors.white, fontSize: 40.0),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- kai_flutter_demo_module/lib/test_page_3.dart
+++ kai_flutter_demo_module/lib/test_page_3.dart
@@ -9,22 +9,52 @@ class TestPage3 extends StatefulWidget {
 }
 
 class _TestPage3State extends State<TestPage3> {
+  int _selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        //导航栏
+        title: Text("Kai Test Page 03"),
+        actions: <Widget>[
+          //导航栏右侧菜单
+          IconButton(icon: Icon(Icons.share), onPressed: () {}),
+        ],
       ),
-      body: Center(
+      drawer: Container(
+        color: Colors.pink,
+        constraints: BoxConstraints.expand(width: 200),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Text:',
-            ),
+          children: [
+            Text("Drawer"),
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        // 底部导航
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+          //悬浮按钮
+          child: Icon(Icons.add),
+          onPressed: _onAdd),
     );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onAdd() {}
 }
